08683fc888c6d22f410736eb054acc56c23371d7
Bharath <118762767+mavbharath@users.noreply.github.com>
2025-08-28 12:38:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+from flask import Flask, jsonify
 
+app = Flask(__name__)
+
+@app.route('/healthz', methods=['GET'])
+def health_check():
+    response = {
+        "status": "ok",
+        "message": "Service is healthy"
+    }
+    return jsonify(response), 200  # Returning JSON with status code 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
